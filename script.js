0a8dad26063bb521315b02f8661a581cdb0e3054
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-10-02 15:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+ function updateSize() {
+      const widthSpan = document.getElementById('width');
+      const heightSpan = document.getElementById('height');
+
+      // Get the window width and height
+      const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+      // Update the content of the <span> elements
+      widthSpan.textContent = width;
+      heightSpan.textContent = height;
+    }
+
+    // Call the updateSize function initially to set the initial size
+    updateSize();
+
+    // Add an event listener to update the size whenever the window is resized
+    window.addEventListener('resize', updateSize);
